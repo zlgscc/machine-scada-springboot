9b9facb398921db9eed16d89055fe0e908cb1777
zlgscc <zhs0311001@gmail.com>
2018-09-11 17:32:37 +0800

    first commit

--- src/main/java/com/example/admin/controller/AdminController.java
+++ src/main/java/com/example/admin/controller/AdminController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AdminController {
 
-    @RequestMapping(value = "/")
+    @RequestMapping(value = "/tologin")
     public String toLogin(){
         return "login";
     }
